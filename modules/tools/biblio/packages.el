f8a94d5193914c9c53b8695dc8935c3667165181
Bruce D'Arcus <bdarcus@gmail.com>
2021-04-03 10:35:54 -0400

    biblio: add bibtex-actions for selectrum

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -6,3 +6,5 @@
   (package! ivy-bibtex :pin "ca09076c3d6e51cc4ffe208c8176fccf3710fcc6"))
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "ca09076c3d6e51cc4ffe208c8176fccf3710fcc6"))
+(when (featurep! :completion selectrum)
+  (package! bibtex-actions :pin "12f3b02bcb88062c8f087b6c35ca22414afa35a5")
