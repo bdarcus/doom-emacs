7fca53f9593296e05a1fb5df663dec5554a1c071
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-13 18:10:48 -0400

    Add bibtex-actions

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -8,3 +8,5 @@
   (package! ivy-bibtex :pin "9f6ea920a49457d85096caa0e61f086a42b2908e"))
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "9f6ea920a49457d85096caa0e61f086a42b2908e"))
+(when (featurep! :completion vertico)
+  (package! bibtex-actions :pin "48b0e046ac44dc17979e62e2350764534bb12e99"))
